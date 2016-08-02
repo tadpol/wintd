c85afbb8d585dfd1fab1cf19df4b5cfd1f23e111
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-08-02 15:03:26 -0500

    Add debug params; if nil then 0.

--- endpoints/get_-v1-tilt.lua
+++ endpoints/get_-v1-tilt.lua
@@ -3,7 +3,10 @@ local qq = TSQ.q():fields('MEAN(temp)'):from('wintd')
 qq:where_tag_is('sn', 3):OR_tag_is('sn', 5)
 qq:AND_time_ago('1h')
 qq:groupby('sn'):groupbytime('15m'):fill('prev'):limit(3)
+
+if request.parameters.qr ~=nil then return tostring(qq) end
 local out = Timeseries.query{ epoch='ms', q = tostring(qq) }
+if request.parameters.raw ~= nil then return out end
 
 if out.results[1].series == nil then
 	-- ERROR
@@ -12,8 +15,8 @@ if out.results[1].series == nil then
 else
 	local wnd, msh = 0, 0
 	for i, window, midish in TSQ.series_ipairs(out.results[1].series) do
-		wnd = wnd + window.mean
-		msh = msh + midish.mean
+		wnd = wnd + (window.mean or 0)
+		msh = msh + (midish.mean or 0)
 	end
 	return (wnd/3) - (msh/3)
 	-- when >0;  window is hotter
