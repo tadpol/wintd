c8c82ff2ed8678523ca77e60b384883b1cb8d551
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-08-02 08:49:50 -0500

    Change from CSV to JSON; Status Board 2 format.

--- endpoints/get_-v1-tilted.lua
+++ endpoints/get_-v1-tilted.lua
@@ -7,7 +7,7 @@ qq:AND_time_ago('8h')
 qq:groupby('sn'):groupbytime('15m'):fill('prev')
 
 if request.parameters.qr ~=nil then return tostring(qq) end
-local out = Timeseries.query{ epoch='ms', q = tostring(qq) }
+local out = Timeseries.query{ epoch='s', q = tostring(qq) }
 if request.parameters.raw ~= nil then return out end
 
 if out.results[1].series == nil then
@@ -15,17 +15,44 @@ if out.results[1].series == nil then
   response.code = 500
   response.message = out.message
 else
-	local result = {}
+	local dpwindow = {}
+	local dproom = {}
+	local min=9999
+	local max=0
 	for i, window, midish in TSQ.series_ipairs(out.results[1].series) do
 		if window.mean ~= nil and midish.mean ~= nil then
-			local s = tostring(window.time)
-			s = s .. "," .. tostring(window.mean - midish.mean)
-			s = s .. "," .. window.mean
-			s = s .. "," .. midish.mean
-			result[#result + 1] = s
+			local s = os.date('%Y-%m-%d %H:%M:%S', window.time)
+			local w = {}
+			w.title = s
+			w.value = window.mean
+			dpwindow[#dpwindow + 1] = w
+
+			local r = {}
+			r.title = s
+			r.value = midish.mean
+			dproom[#dproom + 1] = r
+
+			max = math.max(max, window.mean, midish.mean)
+			min = math.min(min, window.mean, midish.mean)
 		end
 	end
-	return table.concat(result, "\n") .. "\n\n"
+
+	local result = {}
+	result.title = "Desk Temperatures"
+	result.datasequences = {}
+	result.datasequences[1] = {
+		title = "Window",
+		datapoints = dpwindow
+	}
+	result.datasequences[2] = {
+		title = "Room",
+		datapoints = dproom
+	}
+	result.yAxis = {
+      minValue = min-1,
+      maxValue = max+1
+    }
+	return {graph=result}
 end
 -- vim: set ai sw=4 ts=4 :
 
