0c3485622bde41b195d8bff7c003c5d915474d33
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-11-04 08:30:39 -0500

    write to TSDB

--- eventhandlers/product.lua
+++ eventhandlers/product.lua
@@ -1,4 +1,5 @@
 --#EVENT device datapoint
+-- luacheck: globals data (magic variable from Murano)
 -- Since various sunflowers have differnet sensors a single product definition
 -- doesn't work.  So each sunflower has a single dataport that carries JSON data.
 -- Then here we just reshape the JSON into the write command.
@@ -18,8 +19,22 @@ local dvs, err = from_json(data.value[2])
 if dvs ~= nil then
 	dvs.sern = nil
 	dvs.sn = nil
+	local dvn = {}
+	for k,v in pairs(dvs) do
+		local g = tonumber(v)
+		if g ~= nil then
+			dvn[k] = g
+		else
+			dvn[k] = v
+		end
+	end
 	local tswq = TSW.write("window", {sn=data.device_sn}, dvs)
 	Timeseries.write({ query = tswq })
+
+	Tsdb.write{
+		tags = {sn = data.device_sn},
+		metrics = dvn,
+	}
 else
 	Keystore.set{key="err." .. tostring(data.device_sn), value = err}
 end
