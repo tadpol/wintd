5dc4b5f16037007c7e61cc4db0e42311d475b5b4
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-11-04 11:35:54 -0500

    using tsdb

--- endpoints/get_-v1-tilt.lua
+++ endpoints/get_-v1-tilt.lua
@@ -1,23 +1,44 @@
 --#ENDPOINT get /v1/tilt
-local qq = TSQ.q():fields('MEAN(temp)'):from('wintd')
-qq:where_tag_is('sn', 3):OR_tag_is('sn', 5)
-qq:AND_time_ago('1h')
-qq:groupby('sn'):groupbytime('15m'):fill('prev'):limit(3)
+-- luacheck: globals request response (magic variables from Murano)
 
-if request.parameters.qr ~=nil then return tostring(qq) end
-local out = Timeseries.query{ epoch='ms', q = tostring(qq) }
-if request.parameters.raw ~= nil then return out end
+local window = Tsdb.query {
+	tags = {sn = '3'},
+	metrics = {'fahrenheit'},
+	relative_start = "-30m",
+	sampling_size = "15m",
+	aggregate = {"avg"},
+	fill = "previous",
+	epoch = 's',
+}
+local room = Tsdb.query {
+	tags = {sn = '5'},
+	metrics = {'fahrenheit'},
+	relative_start = "-30m",
+	sampling_size = "15m",
+	aggregate = {"avg"},
+	fill = "previous",
+	epoch = 's',
+}
 
-if out.results[1].series == nil then
+if request.parameters.raw ~= nil then return {window = window, room = room} end
+if window.error ~= nil then
 	-- ERROR
-  response.code = 500
-  response.message = out.message
+	response.code = 500
+	response.message = {
+		code=500,
+		message = window
+	}
+elseif room.error ~= nil then
+	response.code = 500
+	response.message = {
+		code=500,
+		message = room
+	}
 else
-	local wnd, msh = 0, 0
-	for i, window, midish in TSQ.series_ipairs(out.results[1].series) do
-		wnd = wnd + (window.mean or 0)
-		msh = msh + (midish.mean or 0)
-	end
-	return (wnd/3) - (msh/3)
+	local wnd = window.values[1][2].avg or 0
+	local rom = room.values[1][2].avg or 0
+	return (wnd/3) - (rom/3)
 	-- when >0;  window is hotter
 end
+
+-- vim: set ai sw=2 ts=2 :
